9b9549f37941f63d84c620dca418dc618f0709be
drespana <drespana@google.com>
2023-10-10 14:14:07 -0500

    first draft band model tests

--- index.test.js
+++ index.test.js
@@ -14,7 +14,11 @@ describe('Band, Musician, and Song Models', () => {
 
     test('can create a Band', async () => {
         // TODO - test creating a band
-        expect('NO TEST').toBe('EXPECTED VALUE HERE');
+        const band = await Band.create({
+            name:"Reign",
+            location:"Chicago"
+        })
+        expect(band.name).toBe('Reign');
     })
 
     test('can create a Musician', async () => {
@@ -24,7 +28,14 @@ describe('Band, Musician, and Song Models', () => {
 
     test('can update a Band', async () => {
         // TODO - test updating a band
-        expect('NO TEST').toBe('EXPECTED VALUE HERE');
+        const band = await Band.create({
+            name:"Reign",
+            location:"Chicago"
+        })
+        await band.update({
+            location:"Atlanta"
+        })
+        expect(band.location).toBe('Atlanta');
     })
 
     test('can update a Musician', async () => {
@@ -34,11 +45,20 @@ describe('Band, Musician, and Song Models', () => {
 
     test('can delete a Band', async () => {
         // TODO - test deleting a band
+
+
         expect('NO TEST').toBe('EXPECTED VALUE HERE');
     })
 
     test('can delete a Musician', async () => {
         // TODO - test deleting a musician
-        expect('NO TEST').toBe('EXPECTED VALUE HERE');
+        const band = await Band.create({
+            name:"Reign",
+            location:"Chicago"
+        })
+        const foundBand = Band.findByPk(1);
+        const deletedBand = foundBand.destroy();
+
+        expect(deletedBand).toEqual(1);
     })
 })
