f8c824c6717c81b3d77441281b3ab218d0b1f8bf
jrtweedle1 <tweedle@google.com>
2023-10-10 13:37:16 -0500

    made changes to musician model

--- models/Musician.js
+++ models/Musician.js
@@ -1,7 +1,20 @@
-const {Sequelize, db} = require('../db');
+const {Sequelize, db, DataTypes, Model} = require('../db');
 
 // TODO - define the Musician model
-let Musician;
+class Musician extends Model {}
+
+Musician.init(
+    {
+        name: DataTypes.STRING,
+        instrument: DataTypes.STRING
+    },
+    {
+        sequelize: db,
+        modelName: "Musician"
+    }
+);
+
+db.sync();
 
 module.exports = {
     Musician
