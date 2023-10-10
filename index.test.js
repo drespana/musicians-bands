8a4424346622ec0f72bcc948a86312d9d7098994
Daniel Guerrero <danxguerrero@google.com>
2023-10-10 16:49:29 -0400

    adjusting tests

--- index.test.js
+++ index.test.js
@@ -31,9 +31,10 @@ describe('Band, Musician, and Song Models', () => {
         // TODO - test updating a band
         const band = await Band.findByPk(1)
         const updatedBand = await band.update({
+            name: "Donald Glover",
             location:"Atlanta"
         })
-        expect(updatedBand.location).toBe('Atlanta');
+        expect(updatedBand.name).toBe("Donald Glover");
     })
 
     test('can update a Musician', async () => {
