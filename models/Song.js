a54093c503d8e01d98bbc3ca684b4cae483f2272
Daniel Guerrero <danxguerrero@google.com>
2023-10-10 14:36:13 -0400

    created song model

--- models/Song.js
+++ models/Song.js
@@ -1,7 +1,17 @@
-const {Sequelize, db} = require('../db');
+const {Sequelize, db, Model, DataTypes} = require('../db');
 
 // TODO - define the Song model
-let Song;
+class Song extends Model {};
+
+Song.init({
+    title: DataTypes.STRING,
+    year: DataTypes.INTEGER,
+    length: DataTypes.INTEGER
+},
+{
+    sequelize: db,
+    modelName: "Song"
+});
 
 module.exports = {
     Song
